545d9057a001dbe55f218a79b0ffde06d422cd3f
Tomas Perez Diaz <tomasperezdiaz03@gmail.com>
2024-03-12 14:44:22 -0300

    Cambio

--- testing.js
+++ testing.js
@@ -1,8 +1,3 @@
 const ProductManager = require("./productManager");
 
 const producto = new ProductManager();
-
-console.log(
-  producto.addProduct("lapto", "lenovo", 222, "https://img1.com", "s23h", 20)
-);
-console.log(producto.getProducts());
