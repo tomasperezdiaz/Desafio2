74d8183f1bb66ad7cdcb57726ebff27218f4e9a4
Tomas Perez Diaz <tomasperezdiaz03@gmail.com>
2024-03-19 15:09:08 -0300

    Segundo desafio

--- productManager.js
+++ productManager.js
@@ -1,9 +1,39 @@
+const fs = require("fs");
+
 class ProductManager {
   #products;
+  #path;
   static idProducto = 0;
 
   constructor() {
-    this.#products = [];
+    this.#products = this.#leerProductosInFile();
+    this.#path = "./data/productos.json";
+  }
+
+  #asignarIdProducto() {
+    let id = 1;
+    if (this.#products.length != 0)
+      id = this.#products[this.#products.length - 1].id + 1;
+    return id;
+  }
+
+  #leerProductosInFile() {
+    try {
+      if (fs.existsSync(this.#path))
+        return JSON.parse(fs.readFileSync(this.#path, "utf-8"));
+
+      return [];
+    } catch (error) {
+      console.log(`Ocurrio un error al leer el producto, ${error}`);
+    }
+  }
+
+  #guardarArchivo() {
+    try {
+      fs.writeFileSync(this.#path, JSON.stringify(this.#products));
+    } catch (error) {
+      console.log(`Ocurrio un error al guardar el producto, ${error}`);
+    }
   }
 
   addProduct(title, description, price, thumbnail, code, stock) {
@@ -16,7 +46,7 @@ class ProductManager {
       return `El codigo ${code} ya se encuentra registrado en otro producto`;
 
     ProductManager.idProducto = ProductManager.idProducto + 1;
-    const id = ProductManager.idProducto;
+    const id = this.#asignarIdProducto();
     const nuevoProducto = {
       id,
       title,
@@ -27,6 +57,7 @@ class ProductManager {
       stock,
     };
     this.#products.push(nuevoProducto);
+    this.#guardarArchivo();
 
     return `Producto agregado exitosamente!`;
   }
@@ -39,6 +70,33 @@ class ProductManager {
     const producto = this.#products.find((e) => e.id == id);
     if (producto) return producto;
     else return `No se encontro el producto con id ${id}`;
+  }
+
+  updateProduct(id, objetoUpdate) {
+    let msg = `El producto con id ${id} no existe`;
+
+    const index = this.#products.findIndex((p) => p.id === id);
+
+    if (index !== -1) {
+      const { id, ...rest } = objetoUpdate;
+      this.#products[index] = { ...this.#products[index], ...rest };
+      this.#guardarArchivo();
+      msg = "Producto actualizado";
+    }
+
+    return msg;
+  }
+
+  deleteProduct(id) {
+    let msg = `El producto con id ${id} no existe`;
+    const index = this.#products.findIndex((p) => p.id === id);
+    if (index !== -1) {
+      this.#products = this.#products.filter((p) => p.id !== id);
+      this.#guardarArchivo();
+      msg = "Producto Elminado!";
+    }
+
+    return msg;
   }
 }
 
--- testing.js
+++ testing.js
@@ -1,3 +1,34 @@
 const ProductManager = require("./productManager");
 
 const producto = new ProductManager();
+
+console.log(
+  producto.addProduct(
+    "Coca",
+    "Cola",
+    300,
+    "https://img1.com",
+    "ColaCompany",
+    10
+  )
+);
+console.log(
+  producto.addProduct(
+    "Co2ca",
+    "Cofsfsla",
+    100,
+    "https://img1.com",
+    "ColaCompanys",
+    102
+  )
+);
+console.log(
+  producto.addProduct(
+    "Cocfsfs3a",
+    "C3olssa",
+    30,
+    "https://img1.com",
+    "ColaCompasnys",
+    103
+  )
+);
